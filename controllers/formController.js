fd8d19d5ae4502141a749d991ff9992f45572656
anitha1204 <selvam12042003@gmail.com>
2024-08-26 11:09:43 +0530

    main

--- controllers/formController.js
+++ controllers/formController.js
@@ -1,6 +1,3 @@
-
-
-
 const Company = require('../models/Companyform');
 
 // Create a new company
@@ -74,72 +71,3 @@ exports.deleteCompany = async (req, res) => {
 
 
 
-
-// const Company = require('../models/Companyform');
-
-// // Create a new company
-// exports.createCompany = async (req, res) => {
-//     try {
-//         const company = new Company(req.body);
-//         const savedCompany = await company.save();
-//         res.status(201).json(savedCompany);
-//     } catch (error) {
-//         res.status(400).json({ error: error.message });
-//     }
-// };
-
-// // Get all companies
-// exports.getAllCompanies = async (req, res) => {
-//     try {
-//         const companies = await Company.find({}, 'companyName natureOfCompany'); // Project only needed fields
-//         res.status(200).json(companies);
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// };
-
-
-// // Search companies by name
-// exports.searchCompanies = async (req, res) => {
-//     try {
-//         const { name } = req.query;
-//         const companies = await Company.find({ companyName: new RegExp(name, 'i') });
-//         res.status(200).json(companies);
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// };
-
-// // Get a company by ID
-// exports.getCompanyById = async (req, res) => {
-//     try {
-//         const company = await Company.findById(req.params.id, 'companyName natureOfCompany');
-//         if (!company) return res.status(404).json({ message: 'Company not found' });
-//         res.status(200).json(company);
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// };
-
-
-// // Update a company
-// exports.updateCompany = async (req, res) => {
-//     try {
-//         const updatedCompany = await Company.findByIdAndUpdate(req.params.id, req.body, { new: true });
-//         if (!updatedCompany) return res.status(404).json({ message: 'Company not found' });
-//         res.status(200).json(updatedCompany);
-//     } catch (error) {
-//         res.status(400).json({ error: error.message });
-//     }
-// };
-
-// // Delete a company
-// exports.deleteCompany = async (req, res) => {
-//     try {
-//         const result = await Company.findByIdAndDelete(req.params.id);
-//         if (!result) return res.status(404).json({ message: 'Company not found' });
-//         res.status(200).json({ message: 'Company deleted successfully' });
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// };
--- models/Companyform.js
+++ models/Companyform.js
@@ -1,26 +1,147 @@
+// const mongoose = require('mongoose');
+
+// const companySchema = new mongoose.Schema({
+//     companyName: { type: String, required: true },
+//     address: { type: String },
+//     registeredOfficeAddress: { type: String },
+//     chiefExecutiveName: { type: String, required: true },
+//     designation: { type: String, required: true },
+//     yearOfEstablishment: { type: String, required: true },
+//     telephone: { type: String, required: true },
+//     email: { type: String, required: true, unique: true },
+//     mobile: { type: String },
+//     website: { type: String },
+//     businessActivity: { type: String },
+//     paidUpCapital: { type: String },
+//     numberOfEmployees: { type: String },
+//     annualTurnover: { type: String },
+//     gstNo: { type: String },
+//     natureOfCompany: { type: String },
+//     keyContactPerson: { type: String },
+//     keyContactMobile: { type: String },
+//     secondaryContactPerson: { type: String },
+//     secondaryContactMobile: { type: String }
+// });
+
+// module.exports = mongoose.model('Company', companySchema);
+
+
+
 const mongoose = require('mongoose');
 
 const companySchema = new mongoose.Schema({
-    companyName: { type: String, required: true },
-    address: { type: String },
-    registeredOfficeAddress: { type: String },
-    chiefExecutiveName: { type: String, required: true },
-    designation: { type: String, required: true },
-    yearOfEstablishment: { type: String, required: true },
-    telephone: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    mobile: { type: String },
-    website: { type: String },
-    businessActivity: { type: String },
-    paidUpCapital: { type: String },
-    numberOfEmployees: { type: String },
-    annualTurnover: { type: String },
-    gstNo: { type: String },
-    natureOfCompany: { type: String },
-    keyContactPerson: { type: String },
-    keyContactMobile: { type: String },
-    secondaryContactPerson: { type: String },
-    secondaryContactMobile: { type: String }
+  companyName: { type: String, required: true },
+  address: { type: String, required: true, maxlength: 100 },
+  registeredOfficeAddress: { type: String, required: true, maxlength: 100 },
+  chiefExecutiveName: { type: String, required: true },
+  designation: { type: String, required: true },
+  yearOfEstablishment: { 
+    type: Number, 
+    required: true,
+    min: 1800,
+    max: new Date().getFullYear()
+  },
+  telephone: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{10,12}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid telephone number!`
+    }
+  },
+  email: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid email address!`
+    }
+  },
+  mobile: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{10}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid mobile number!`
+    }
+  },
+  website: { 
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /^https?:\/\/.+/.test(v);
+      },
+      message: props => `${props.value} is not a valid URL!`
+    }
+  },
+  businessActivity: { type: String, required: true },
+  paidUpCapital: { type: Number, required: true, min: 0 },
+  numberOfEmployees: { type: Number, required: true, min: 1 },
+  annualTurnover: { type: Number, required: true, min: 0 },
+  gstNo: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid GST number!`
+    }
+  },
+  natureOfCompany: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^[A-Za-z\s]+$/.test(v);
+      },
+      message: props => `${props.value} should contain only letters!`
+    }
+  },
+  keyContactPerson: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^[A-Za-z\s]+$/.test(v);
+      },
+      message: props => `${props.value} should contain only letters!`
+    }
+  },
+  keyContactMobile: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{10}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid mobile number!`
+    }
+  },
+  secondaryContactPerson: { 
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /^[A-Za-z\s]+$/.test(v);
+      },
+      message: props => `${props.value} should contain only letters!`
+    }
+  },
+  secondaryContactMobile: { 
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /^\d{10}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid mobile number!`
+    }
+  }
 });
 
 module.exports = mongoose.model('Company', companySchema);
