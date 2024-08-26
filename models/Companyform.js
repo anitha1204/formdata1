d6d8ce810a739fb25bcde98178c87d659d00b952
anitha1204 <selvam12042003@gmail.com>
2024-08-26 11:18:50 +0530

    main

--- models/Companyform.js
+++ models/Companyform.js
@@ -27,121 +27,121 @@
 
 
 
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const companySchema = new mongoose.Schema({
-//   companyName: { type: String, required: true },
-//   address: { type: String, required: true, maxlength: 100 },
-//   registeredOfficeAddress: { type: String, required: true, maxlength: 100 },
-//   chiefExecutiveName: { type: String, required: true },
-//   designation: { type: String, required: true },
-//   yearOfEstablishment: { 
-//     type: Number, 
-//     required: true,
-//     min: 1800,
-//     max: new Date().getFullYear()
-//   },
-//   telephone: { 
-//     type: String, 
-//     required: true,
-//     validate: {
-//       validator: function(v) {
-//         return /^\d{10,12}$/.test(v);
-//       },
-//       message: props => `${props.value} is not a valid telephone number!`
-//     }
-//   },
-//   email: { 
-//     type: String, 
-//     required: true,
-//     validate: {
-//       validator: function(v) {
-//         return /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(v);
-//       },
-//       message: props => `${props.value} is not a valid email address!`
-//     }
-//   },
-//   mobile: { 
-//     type: String, 
-//     required: true,
-//     validate: {
-//       validator: function(v) {
-//         return /^\d{10}$/.test(v);
-//       },
-//       message: props => `${props.value} is not a valid mobile number!`
-//     }
-//   },
-//   website: { 
-//     type: String,
-//     validate: {
-//       validator: function(v) {
-//         return /^https?:\/\/.+/.test(v);
-//       },
-//       message: props => `${props.value} is not a valid URL!`
-//     }
-//   },
-//   businessActivity: { type: String, required: true },
-//   paidUpCapital: { type: Number, required: true, min: 0 },
-//   numberOfEmployees: { type: Number, required: true, min: 1 },
-//   annualTurnover: { type: Number, required: true, min: 0 },
-//   gstNo: { 
-//     type: String, 
-//     required: true,
-//     validate: {
-//       validator: function(v) {
-//         return /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/.test(v);
-//       },
-//       message: props => `${props.value} is not a valid GST number!`
-//     }
-//   },
-//   natureOfCompany: { 
-//     type: String, 
-//     required: true,
-//     validate: {
-//       validator: function(v) {
-//         return /^[A-Za-z\s]+$/.test(v);
-//       },
-//       message: props => `${props.value} should contain only letters!`
-//     }
-//   },
-//   keyContactPerson: { 
-//     type: String, 
-//     required: true,
-//     validate: {
-//       validator: function(v) {
-//         return /^[A-Za-z\s]+$/.test(v);
-//       },
-//       message: props => `${props.value} should contain only letters!`
-//     }
-//   },
-//   keyContactMobile: { 
-//     type: String, 
-//     required: true,
-//     validate: {
-//       validator: function(v) {
-//         return /^\d{10}$/.test(v);
-//       },
-//       message: props => `${props.value} is not a valid mobile number!`
-//     }
-//   },
-//   secondaryContactPerson: { 
-//     type: String,
-//     validate: {
-//       validator: function(v) {
-//         return /^[A-Za-z\s]+$/.test(v);
-//       },
-//       message: props => `${props.value} should contain only letters!`
-//     }
-//   },
-//   secondaryContactMobile: { 
-//     type: String,
-//     validate: {
-//       validator: function(v) {
-//         return /^\d{10}$/.test(v);
-//       },
-//       message: props => `${props.value} is not a valid mobile number!`
-//     }
-//   }
-// });
+const companySchema = new mongoose.Schema({
+  companyName: { type: String, required: true },
+  address: { type: String, required: true, maxlength: 100 },
+  registeredOfficeAddress: { type: String, required: true, maxlength: 100 },
+  chiefExecutiveName: { type: String, required: true },
+  designation: { type: String, required: true },
+  yearOfEstablishment: { 
+    type: Number, 
+    required: true,
+    min: 1800,
+    max: new Date().getFullYear()
+  },
+  telephone: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{10,12}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid telephone number!`
+    }
+  },
+  email: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid email address!`
+    }
+  },
+  mobile: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{10}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid mobile number!`
+    }
+  },
+  website: { 
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /^https?:\/\/.+/.test(v);
+      },
+      message: props => `${props.value} is not a valid URL!`
+    }
+  },
+  businessActivity: { type: String, required: true },
+  paidUpCapital: { type: Number, required: true, min: 0 },
+  numberOfEmployees: { type: Number, required: true, min: 1 },
+  annualTurnover: { type: Number, required: true, min: 0 },
+  gstNo: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid GST number!`
+    }
+  },
+  natureOfCompany: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^[A-Za-z\s]+$/.test(v);
+      },
+      message: props => `${props.value} should contain only letters!`
+    }
+  },
+  keyContactPerson: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^[A-Za-z\s]+$/.test(v);
+      },
+      message: props => `${props.value} should contain only letters!`
+    }
+  },
+  keyContactMobile: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{10}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid mobile number!`
+    }
+  },
+  secondaryContactPerson: { 
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /^[A-Za-z\s]+$/.test(v);
+      },
+      message: props => `${props.value} should contain only letters!`
+    }
+  },
+  secondaryContactMobile: { 
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /^\d{10}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid mobile number!`
+    }
+  }
+});
 
-// module.exports = mongoose.model('Company', companySchema);
+module.exports = mongoose.model('Company', companySchema);
