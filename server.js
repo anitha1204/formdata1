f3e3983f5b3bbc12f653c1473894cc40cb80b154
anitha1204 <selvam12042003@gmail.com>
2024-09-09 13:31:09 +0530

    main

--- server.js
+++ server.js
@@ -62,4 +62,4 @@ app.use(errorHandler);
 app.use(handleError);
 
 const PORT = 9000;
-app.liste(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
